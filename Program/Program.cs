73bc6d1e26bf1aacb630098a0f0f8ea506c6e7ff
Augusto Taño <augusto.1812@hotmail.com>
2020-12-11 20:34:03 -0300

    New Project

---
+++ Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PII_EXAMEN_ROLEPLAY_ENDGAME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
